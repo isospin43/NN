5979bff438bdbea649af67c7df9e2f07a82cba36
Steve Vayl <Stevenvayl43@gmail.com>
2022-04-17 21:05:42 -0400

    Create main.tf

---
+++ learning/main.tf
@@ -0,0 +1,13 @@
+provider "aws" {
+  region = "us-east-1"
+}
+
+# define resource for instance
+resource "aws_instance" "web" {
+  ami = ""
+  subnet_id = ""
+  instance_type = "t3.micro"
+  tags {
+    name="sv-test"
+  }
+}
