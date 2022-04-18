4aff1090b5fe31675bbe8280a8d6d87ccbb4f866
Steve Vayl <Stevenvayl43@gmail.com>
2022-04-17 21:20:01 -0400

    Update main.tf

--- learning/main.tf
+++ learning/main.tf
@@ -4,8 +4,8 @@ provider "aws" {
 
 # define resource for instance
 resource "aws_instance" "web" {
-  ami = ""
-  subnet_id = ""
+  ami = "ami-0a3c14e1ddbe7f23c"
+  subnet_id = "subnet-0d58cc5c983862a26"
   instance_type = "t3.micro"
   tags = {
     name="sv-test"
